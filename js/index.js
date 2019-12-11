70a4852c0a9f9c1da2380353468ba42e7df24185
Samuel Rowan <samuel@samuelrowan.com>
2019-12-10 20:27:21 -0600

    Working on it

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+// mouseover
+// keydown
+// wheel
+// drag / drop
+// load
+// focus
+// resize
+// scroll
+// select
+// dblclick
+document.getElementById("nav-link").addEventListener("mouseover", (event) => {
+  alert("it works")
+});
