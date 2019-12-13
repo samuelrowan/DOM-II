dec591d7f02858f74d512ff8985b1ef56a743b1e
Samuel Rowan <samuel@samuelrowan.com>
2019-12-12 20:10:02 -0600

    finished

--- js/index.js
+++ js/index.js
@@ -9,6 +9,55 @@
 // scroll
 // select
 // dblclick
-document.getElementById("nav-link").addEventListener("mouseover", (event) => {
-  alert("it works")
+const introH2 = document.querySelector('.intro h2')
+const keyPress = document.querySelector('html')
+const keyUp = document.querySelector('.intro')
+const busInSand = document.querySelector('#busInSand');
+const doubleClick = document.querySelector('.intro h2')
+const mouseDown = document.querySelector('.intro h2')
+const mouseUp = document.querySelector('.intro h2')
+const mouseMove = document.querySelector('.intro h2')
+const mouseEnter = document.querySelector('.intro h2')
+const mouseLeave = document.querySelector('.intro h2')
+
+console.log(busInSand)
+
+introH2.addEventListener('mouseover', function(){
+    introH2.style.color = 'purple';
+});
+
+keyPress.addEventListener('keydown', function(event){
+    console.log(`${event}`)
+})
+
+keyUp.addEventListener('keyup', function(event){
+    console.log(`${event} again`);
+});
+
+busInSand.addEventListener('click', function(){
+    console.log(`clickety clackety`);
+})
+
+doubleClick.addEventListener('dblclick', function(){
+    console.log(`clickety clickety clackety`);
+});
+
+mouseDown.addEventListener('mousedown', function(){
+    console.log(`CL`);
+});
+
+mouseUp.addEventListener('mouseup', function(){
+    console.log(`ICK`);
+});
+
+mouseMove.addEventListener('mousemove', function(){
+    console.log(`weeeeeeeeeee`);
+});
+
+mouseEnter.addEventListener('mouseenter', function(){
+    console.log(`Im here`);
+});
+
+mouseLeave.addEventListener('mouseleave', function(){
+    console.log(`see ya later`);
 });
