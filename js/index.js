0f94882cdcf4c2adcae71fa2c2dba6244b264f3d
Samuel Rowan <samuel@samuelrowan.com>
2019-12-13 19:55:45 -0600

    a touch drier

--- js/index.js
+++ js/index.js
@@ -14,11 +14,7 @@ const keyPress = document.querySelector('html')
 const keyUp = document.querySelector('.intro')
 const busInSand = document.querySelector('#busInSand');
 const doubleClick = document.querySelector('.intro h2')
-const mouseDown = document.querySelector('.intro h2')
-const mouseUp = document.querySelector('.intro h2')
-const mouseMove = document.querySelector('.intro h2')
-const mouseEnter = document.querySelector('.intro h2')
-const mouseLeave = document.querySelector('.intro h2')
+
 
 console.log(busInSand)
 
@@ -42,22 +38,22 @@ doubleClick.addEventListener('dblclick', function(){
     console.log(`clickety clickety clackety`);
 });
 
-mouseDown.addEventListener('mousedown', function(){
+introH2.addEventListener('mousedown', function(){
     console.log(`CL`);
 });
 
-mouseUp.addEventListener('mouseup', function(){
+introH2.addEventListener('mouseup', function(){
     console.log(`ICK`);
 });
 
-mouseMove.addEventListener('mousemove', function(){
+introH2.addEventListener('mousemove', function(){
     console.log(`weeeeeeeeeee`);
 });
 
-mouseEnter.addEventListener('mouseenter', function(){
+introH2.addEventListener('mouseenter', function(){
     console.log(`Im here`);
 });
 
-mouseLeave.addEventListener('mouseleave', function(){
+introH2.addEventListener('mouseleave', function(){
     console.log(`see ya later`);
 });
